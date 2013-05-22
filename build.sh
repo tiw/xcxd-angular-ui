35b55cb732d6c461c4474e0dfb449f56f4b4687c
Ting Wang <tting.wang@gmail.com>
2013-05-22 15:57:49 +0800

    Copy needed js.

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+cp ./components/angular/angular.js javascripts
+cp ./components/bootstrap/js/bootstrap-tooltip.js javascripts
+cp ./components/bootstrap/js/bootstrap-popover.js javascripts
+cp ./components/bootstrapx-clickover/js/bootstrapx-clickover.js javascripts
+cp ./components/d3/d3.js javascripts
+
